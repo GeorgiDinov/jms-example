1c83ae70a32ece4917f9ae467a605919c607de6b
Georgi Dinov <georgidinov1@abv.bg>
2021-03-12 22:40:44 +0200

    Initial commit

---
+++ src/test/java/com/georgidinov/jmsexample/JmsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgidinov.jmsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
